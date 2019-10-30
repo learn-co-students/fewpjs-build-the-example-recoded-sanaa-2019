ba0119f8d69f2fb09441087ff80c48bf31e2cbc6
EmanAlzariey <eman_alzariey@yahoo.com>
2019-10-30 01:29:22 -0700

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 
-console.log(FULL_HEART)
+// console.log(FULL_HEART)
 // Your JavaScript code goes here!
 
 
@@ -34,92 +34,103 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
 let error = document.getElementById("modal")
 error.className= "hidden"
 
+;
+
+      //I need to fetch the info in db.json 
+
+      fetch("http://localhost:3000/likes")
+      // .then(resp => resp.json())
+      .then(data => {//console.log(data)
+       
+
+
+        let heart = document.querySelectorAll(".like-glyph"); //console.log(heart) //node list (use for..in)
+      //get the heart from here
+        for(const element in data){//console.log(data[element]["like"])
+          let data2 = data[element]["like"];
+          heart[element].innerHTML = data2;
+        //  heart[element].style.color = "red";
+
+          console.log(heart[element].innerHTML)
+
+         if( heart[element].innerHTML == FULL_HEART){
+           //to add a second class (.add) and get the color red from activated heart (without messing with the style)
+          heart[element].classList.add("activated-heart");
+       
+         }else {
+          heart[element].classList.remove("activated-heart")
+      
+         }
+       
+          
+        }
+
+        }) //after then.. now catch error when we call mimicServerCall>> it always return FAILIUR 
+        .catch(function(err) {
+          //remove error model(which means add hidden) after 5 seconds = 5000
+          setTimeout(function() {error2.classList.add("hidden")} , 5000);
+
+          let error2 = document.getElementById("modal");
+          error2.classList.remove("hidden")
+          console.log(error2.className)
+          console.log(err.message);
+        
+          let errorMessage = document.getElementById("modal-message")
+          return errorMessage.innerHTML = err.message;
+       
+          
+         
+        })//end catch
+         
+
 // click on heart 
 
 let like = document.querySelectorAll("li")
 
 
-like.forEach(function(e) {
+ like.forEach(function(e) {
     e.addEventListener("click", function(e) {
-        //this function does stuff
+//         //this function does stuff
         console.log("you can click more than once")
 
       if (e.target && e.target.matches(".like-glyph")){
 
-       // e.target.style.color  = "red" //save this at the server using fetch "POST"
+//        // e.target.style.color  = "red" //save this at the server using fetch "POST"
 
-       // get the heart sign from html
+//        // get the heart sign from html
        let heart = document.querySelector(".like-glyph")
-      console.log("This heart" +  heart.innerHTML)
+      //  console.log("This heart" +  heart.innerHTML)
 
        if( e.target.innerHTML == EMPTY_HEART){
         e.target.innerHTML = FULL_HEART;
-        e.target.style.color = "red"
-        // e.target.className = "activated-heart";
+       // e.target.style.color = "red" //dont use style 
+       e.target.classList.add("activated-heart");
 
-      }else { 
-        console.log("removed")
-        e.target.innerHTML = EMPTY_HEART;
-        e.target.style.color  = "black";
+       }else { 
+         console.log("removed")
+         e.target.innerHTML = EMPTY_HEART;
+         e.target.classList.remove("activated-heart");
+        // e.target.style.color  = "black";
       }
-       
-//////////fetch here////////////////
 
+            ////////NEW FETCH PATCH
+            let newlike = {
+              like : e.target.innerHTML
+            }
 
-// submitData( e.target.innerHTML);
-// console.log("New data"+ e.target.innerHTML)
+            
+          //request to sever to update using fetch PATCH 
 
-// function submitData( likes ){
+          fetch("http://localhost:3000/likes/201811189", {
+              method: "PATCH",
+              headers: {"content-Type": "application/json", "Accept":"application/json"},
+              body: JSON.stringify(newlike)
+          })
 
-//   let formData = {
-//      like: likes
-//     };
+      }//end if 
+
+ 
+
+   })//end event listener
+   })//end for each 
      
-//     let configObj = {
-//       method: "PATCH",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Accept": "application/json"
-//       },
-//       body: JSON.stringify(formData)
-//     };
-     
-//     return fetch(" http://localhost:3000/likes/1", configObj)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(object) {
-//       console.log(object);
-//       // return document.body.innerHTML = object.id
-//     })
-//     .catch(function(error) {
-//       alert("Bad things! Ragnarők!");
-//       console.log(error.message);
-  
-      
-//       // return document.body.innerHTML = error.message;
-  
-//     });
-    
-//   }
-
-////////NEW FETCH
-let newlike = {
-  like : e.target.innerHTML
-
- }
- //request to sever to update using fetch PATCH
-
- fetch("http://localhost:3000/likes/1", {
-    method: "PATCH",
-    headers: {"content-Type": "application/json", "Accept":"application/json"},
-    body: JSON.stringify(newlike)
- })
-  
-  
-    }
-
-    // You can write these comments to yourself 
-    //now I need to applay the catch alert message
-    });
-});
