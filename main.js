81b3f299ab1c5becf5482f8281ba84ce872a6b34
learn <mohammedazizbuss@gmail.com>
2019-11-11 00:44:34 -0800

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let errorModal=document.getElementById("modal");
+let like=document.getElementsByClassName("like-glyph");
+for(ele of like){
+  let chech;
+  ele.addEventListener('click',function(){
+    let a=this;
+    console.log(mimicServerCall().then(resp=>resp).then(function(json){
+      console.log("Hi resp "+json);
+      if(a.classList=="like-glyph"){
+        a.innerText=FULL_HEART;
+        a.classList.add("activated-heart");
+      }
+      else if (a.classList=="like-glyph activated-heart"){
+        a.innerText=EMPTY_HEART;
+        a.classList.remove("activated-heart");
+      }
+    }).catch(function(error){
+      console.log('Hi Error, '+error);
+      errorModal.classList.remove("hidden");
+      errorModal.innerText+=" "+error;
+      setTimeout(function(){
+        errorModal.classList.add("hidden");
+      },5000);
+
+    }));
+  })
+}
 
 
 
