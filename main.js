13b048fec18f5d5aac8cc9b183f4138582e380be
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2019-11-15 09:55:42 -0800

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.getElementById("modal").className="hidden"
+fetch('http://localhost:3000/likes')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+let likeIcon=document.querySelector('.like-glyph')
+likeIcon.addEventListener('click',function(){
 
+if(likeIcon.innerHTML==FULL_HEART){
+  likeIcon.innerHTML=EMPTY_HEART
+  likeIcon.className=likeIcon.className.replace("activated-heart","")
+console.log("full")
+}
+else {
+  likeIcon.className="activated-heart"
+likeIcon.innerHTML=FULL_HEART
+console.log("Empty")
+}
+  console.log("it works")
+})
+.catch(function(error){
+  console.log(error);
+  // console.log("error");
+  let modal = document.querySelector("#modal");
+  modal.className = modal.className.replace("hidden","")
+  setTimeout(() => {
+    modal.className = modal.className.replace("","hidden")
 
-
+  }, 5000);
+})
+  
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
