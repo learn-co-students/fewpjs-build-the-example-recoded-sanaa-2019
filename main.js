ece789649f1af059e95db2fa6314128791a68ab6
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 17:23:39 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let mod=document.getElementById('modal').className='hidden';
+mod.addEventListener('onload',function(){
 
+})
 
+let like=document.querySelector('.like-glyph')
+try{
+like.addEventListener('click',function(){
+if(like.innerHTML==FULL_HEART){
+  like.innerHTML=EMPTY_HEART
+  like.className=like.className.replace("activated-heart","")
+}
+else {
+like.className=like.className.replace("","activated-heart")
+like.innerHTML=FULL_HEART
+like.style.color='red'
 
+}
+})
+}
+catch(e){
+  console.log(e);
+  let remove=document.querySelector('#modal');
+  setTimeout(()=>{
+     remove.className=remove.className.replace("hidden","")
+  },5000)
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
